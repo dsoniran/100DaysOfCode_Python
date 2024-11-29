9fd9032769a625d834b78d1f7b463ef6fc709ebc
Dáre <78746521+dsoniran@users.noreply.github.com>
2024-11-29 19:41:49 +0000

    Update and rename execise_1.py to ex1_printingPractice.py

--- Day1/ex1_printingPractice.py
+++ Day1/ex1_printingPractice.py
@@ -1,4 +1,5 @@
-"""Printing Practice
+"""
+Printing Practice
 Write a program that uses print statements to print the following recipe into the Output console. 
 The text to print is already there, you just need to make it into code. 
 Your code should print all five lines exactly the same as the example output below. 
