943d46372f0c6789cb5b803b48e257ff9df241ee
Dáre <78746521+dsoniran@users.noreply.github.com>
2024-11-29 20:59:41 +0000

    fixup

--- Day2_tipCalculator.py
+++ Day2_tipCalculator.py
@@ -1,10 +1,10 @@
 print("Welcome to the tip calculator!")
 
-bill = int(input("what was the total bill? "))
-tip = int(input("How much tip would you like to give? 10, 12, or 15 "))
+bill = int(input("What was the total bill? £"))
+tip = int(input("How much tip would you like to give? 10, 12 or 15 "))
 total_people = int(input("How many people to split the bills? "))
 
-cost = bill + (bill/100*tip)
+cost = bill + (tip/100*bill)
 cost_pp = cost/total_people
 
 print(f"Each person should pay: £{round(cost_pp, 2)}")
